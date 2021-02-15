303236fc0493c32c0701e348fe31463d8e46218c
rohit__143 <rohitkumpan01@gmail.com>
2021-02-16 01:33:09 +0530

    done

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -14,17 +14,21 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('URL')
+    console.log(postId);
+    fetch('http://localhost:8080/feed/post/'+postId)
       .then(res => {
         if (res.status !== 200) {
+          console.log(postId);
           throw new Error('Failed to fetch status');
         }
+        console.log(postId);
         return res.json();
       })
       .then(resData => {
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
+          image:'http://localhost:8080/'+resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content
         });
